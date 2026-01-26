71cf30f7a24d0b278c30991b680645f9251bfb54
seongjinb652-wq <seongjinb652@gmail.com>
2026-01-26 18:04:00 +0900

    TARGET_DIR = Path("C:/Users/USER/Downloads/@@@인도네시아PDT암센터FS")

--- src/batch/config_local.py
+++ src/batch/config_local.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 class Settings:
     # 로컬 테스트용 디렉토리
-    TARGET_DIR = Path(r"C:\Users\USER\Downloads\@@@인도네시아PDT암센터FS")
+    TARGET_DIR = Path(r"C:/Users/USER/Downloads/@@@인도네시아PDT암센터FS")
 
     # 상태 파일 저장 위치
     STATE_FILE = Path("batch_state_local.json")
